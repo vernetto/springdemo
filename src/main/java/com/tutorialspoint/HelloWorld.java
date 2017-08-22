7c4a6719a1d71a3df0c1472c2021f87b062d39a9
vernetto <publicpierre@gmail.com>
2017-08-22 21:07:18 +0200

    init method

--- src/main/java/com/tutorialspoint/HelloWorld.java
+++ src/main/java/com/tutorialspoint/HelloWorld.java
@@ -10,4 +10,9 @@ public class HelloWorld {
     public void getMessage(){
         System.out.println("Your Message : " + message);
     }
+
+    public void init() {
+        System.out.println("init called!");
+    }
+
 }
