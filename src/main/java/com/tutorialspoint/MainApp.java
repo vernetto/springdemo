e76a2a4f70e45eaa11869a16d786e9bfdf3de006
vernetto <publicpierre@gmail.com>
2017-08-22 20:44:42 +0200

    singleton

--- src/main/java/com/tutorialspoint/MainApp.java
+++ src/main/java/com/tutorialspoint/MainApp.java
@@ -6,7 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-        obj.getMessage();
+        HelloWorld obj1 = (HelloWorld) context.getBean("helloWorld");
+        obj1.getMessage();
+
+        HelloWorld obj2 = (HelloWorld) context.getBean("helloWorld");
+        obj2.getMessage();
+        
     }
 }
