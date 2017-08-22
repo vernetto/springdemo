40a80f37b527071ddcbf99a08b2ee0ff4dc7c942
vernetto <publicpierre@gmail.com>
2017-08-23 01:23:29 +0200

    destroy 2

--- README.md
+++ README.md
@@ -9,4 +9,10 @@ git checkout v003 // 2 beans, singleton
 git checkout v004 // 2 beans, prototype
 
 git checkout v005 // init method
+
+git checkout v006_1 // destroy not working
+
+git checkout v006_2 // destroy working
+
+
   
--- src/main/java/com/tutorialspoint/MainApp.java
+++ src/main/java/com/tutorialspoint/MainApp.java
@@ -1,17 +1,19 @@
 package com.tutorialspoint;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
         HelloWorld obj1 = (HelloWorld) context.getBean("helloWorld");
         obj1.getMessage();
 
         HelloWorld obj2 = (HelloWorld) context.getBean("helloWorld");
         obj2.getMessage();
 
+        context.registerShutdownHook();
 
     }
 }
