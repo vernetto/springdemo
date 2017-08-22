85b3fd39564a59c31d6029fe51ab665a500257fd
vernetto <publicpierre@gmail.com>
2017-08-22 05:38:54 +0200

    first cut

---
+++ src/main/java/com/tutorialspoint/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.tutorialspoint;
+
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message){
+        System.out.println("setting message to " + message);
+        this.message  = message;
+    }
+    public void getMessage(){
+        System.out.println("Your Message : " + message);
+    }
+}
